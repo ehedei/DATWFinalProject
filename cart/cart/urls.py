532cd5dcedd0c247d49e80ee38235a3dd4428cf0
unknown <ehedeihg@gmail.com>
2020-10-07 19:43:42 +0100

    Name changes

--- cart/cart/urls.py
+++ cart/cart/urls.py
@@ -1,4 +1,4 @@
-"""chart URL Configuration
+"""cart URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
--- cart/cart/wsgi.py
+++ cart/cart/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for chart project.
+WSGI config for cart project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chart.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cart.settings')
 
 application = get_wsgi_application()
