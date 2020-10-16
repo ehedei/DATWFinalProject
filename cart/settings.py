b2790281c39b747b402f283dfb0c6449ef62784e
unknown <ehedeihg@gmail.com>
2020-10-16 16:49:39 +0100

    Cambios para heroku

--- cart/settings.py
+++ cart/settings.py
@@ -108,8 +108,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, '/ootb/static/')
 STATICFILES_DIRS = [
-     os.path.join(BASE_DIR, "static"),
-
+     "static",
 ]
 
 AUTH_USER_MODEL = 'shoppingcart.User'
