e565db8cc23e5bf76a8c2c33d322afc7aebd1d5f
unknown <ehedeihg@gmail.com>
2020-10-16 15:28:13 +0100

    Cambios para produccion

--- cart/settings.py
+++ cart/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -19,11 +19,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '8q+)=%t!p3efx4=n8ix(6zy0(*vqwib=bx6285zq0o62jot-73'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '8q+)=%t!p3efx4=n8ix(6zy0(*vqwib=bx6285zq0o62jot-73')
+DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
 ALLOWED_HOSTS = []
 
